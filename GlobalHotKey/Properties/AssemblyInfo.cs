8e01d416144285fdcabae36973ecdbf1b454f58f
kirmir <kirmiro@gmail.com>
2012-07-15 12:05:49 +0300

    Updated assembly info and installed library into test application as nuget package.

--- GlobalHotKey/Properties/AssemblyInfo.cs
+++ GlobalHotKey/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GlobalHotKey")]
-[assembly: AssemblyDescription("Provides an ability to setup system-wide hot keys and react on their events.")]
+[assembly: AssemblyDescription("Provides an easy way to setup system-wide hot keys and react on their events")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GlobalHotKey")]
